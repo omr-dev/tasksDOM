b573e2e45ef19dbbc6283015e573ba95d49d9c73
omer <peynirciomerfaruk@gmail.com>
2022-03-29 17:37:05 +0200

    task 3 focus added

--- 3/app.js
+++ 3/app.js
@@ -1,5 +1,5 @@
 const inputElem = document.querySelector("#toClick");
 const parElem = document.querySelector("p");
-inputElem.addEventListener("click", () => {
+inputElem.addEventListener("focus", () => {
   parElem.style.backgroundColor = "red";
 });
